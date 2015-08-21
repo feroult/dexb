d4783b86f241c12af6a9b32e752c3a11d8011cf3
feroult <feroult@gmail.com>
2015-08-21 13:30:20 -0300

    pure d3

--- web/src/main/webapp/static/js/product.js
+++ web/src/main/webapp/static/js/product.js
@@ -1,97 +1,125 @@
 (function () {
 
-    function productChart(_width, _height) {
+    function productChart(width, height) {
+        var dim = defineChartDimentions(width, height);
+        var svg = createChartSVG(dim);
 
+        d3.tsv("data/product.tsv", type, function (error, data) {
+            var x = createX(dim, svg, data);
+            var y0 = createY0(dim, svg, data);
+            var y1 = createY1(dim, svg, data);
+
+            createBars(dim, svg, data, x, y0);
+        });
+    }
+    
+    function defineChartDimentions(width, height) {
         var margin = {
-                top: 80,
-                right: 80,
-                bottom: 80,
-                left: 80
-            },
-            width = _width - margin.left - margin.right,
-            height = _height - margin.top - margin.bottom;
+            top: 80,
+            right: 80,
+            bottom: 80,
+            left: 80
+        };
 
+        return {
+            width: width - margin.left - margin.right,
+            height: height - margin.top - margin.bottom,
+            margin: margin
+        }
+    }
+
+    function createChartSVG(dim) {
+        return d3.select("#product-chart").append("svg")
+            .attr("width", dim.width + dim.margin.left + dim.margin.right)
+            .attr("height", dim.height + dim.margin.top + dim.margin.bottom)
+            .append("g")
+            .attr("class", "graph")
+            .attr("transform", "translate(" + dim.margin.left + "," + dim.margin.top + ")");
+    }
+
+    function createX(dim, svg, data) {
         var x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .3);
-
-        var y0 = d3.scale.linear().domain([300, 1100]).range([height, 0]),
-            y1 = d3.scale.linear().domain([20, 80]).range([height, 0]);
+            .rangeRoundBands([0, dim.width], .3)
+            .domain(data.map(function (d) {
+                return d.year;
+            }));
 
         var xAxis = d3.svg.axis()
             .tickSize(0)
             .scale(x)
             .orient("bottom");
 
-        // create left yAxis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + (dim.height + 6) + ")")
+            .call(xAxis);
+
+        return x;
+    }
+
+    function createY0(dim, svg, data) {
+        var y0 = d3.scale.linear().domain([300, 1100]).range([dim.height, 0])
+            .domain([0, d3.max(data, function (d) {
+                return d.money;
+            })]);
+
         var yAxisLeft = d3.svg.axis()
             .scale(y0)
             .ticks(4)
             .outerTickSize(0)
             .orient("left");
-        
-        // create right yAxis
+
+        svg.append("g")
+            .attr("class", "y axis axisLeft")
+            .attr("transform", "translate(0,0)")
+            .call(yAxisLeft);
+
+        return y0;
+    }
+
+    function createY1(dim, svg, data) {
+        var y1 = d3.scale.linear().domain([20, 80]).range([dim.height, 0]);
+
         var yAxisRight = d3.svg.axis()
             .scale(y1)
             .ticks(6)
             .outerTickSize(0)
             .orient("right");
 
-        var svg = d3.select("#product-chart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("class", "graph")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        svg.append("g")
+            .attr("class", "y axis axisRight")
+            .attr("transform", "translate(" + (dim.width) + ",0)")
+            .call(yAxisRight);
 
-        d3.tsv("data/product.tsv", type, function (error, data) {
-            x.domain(data.map(function (d) {
-                return d.year;
-            }));
-            y0.domain([0, d3.max(data, function (d) {
-                return d.money;
-            })]);
+        return y1;
+    }
 
+    function createBars(dim, svg, data, x, y0) {
+        var bars = svg.selectAll(".bar").data(data).enter();
 
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + (height + 6) + ")")
-                .call(xAxis);
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) {
+                return x(d.year);
+            })
+            .attr("width", x.rangeBand())
 
-            svg.append("g")
-                .attr("class", "y axis axisLeft")
-                .attr("transform", "translate(0,0)")
-                .call(yAxisLeft)
+        .attr("y", dim.height)
+            .attr("height", 0)
 
-            svg.append("g")
-                .attr("class", "y axis axisRight")
-                .attr("transform", "translate(" + (width) + ",0)")
-                .call(yAxisRight)
+        .transition()
+            .delay(function (d, i) {
+                return i * 100;
+            })
 
-            bars = svg.selectAll(".bar").data(data).enter();
+        .attr("y", function (d) {
+                return y0(d.money);
+            })
+            .attr("height", function (d, i, j) {
+                return dim.height - y0(d.money);
+            });
 
-            bars.append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) {
-                    return x(d.year);
-                })
-                .attr("width", x.rangeBand())
-
-            .attr("y", height)
-                .attr("height", 0)
-
-            .transition()
-                .delay(function (d, i) {
-                    return i * 100;
-                })
-
-            .attr("y", function (d) {
-                    return y0(d.money);
-                })
-                .attr("height", function (d, i, j) {
-                    return height - y0(d.money);
-                });
-
-        });
+        return bars;
     }
 
     function type(d) {
@@ -100,4 +128,5 @@
     }
 
     productChart(800, 400);
+
 })();
